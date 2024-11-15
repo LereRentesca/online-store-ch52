451b4497aaf2e24aa5696effea9af458c04240bc
LereRentesca <esteban.renteria@uabc.edu.mx>
2024-11-14 20:52:27 -0800

    Class 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar.jsx";
+import Footer from './components/footer.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='App'>
+      <Navbar></Navbar>
+      <div className='content'>
+        <h1>Hello World React!</h1>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import "./styles/footer.css";
+
+function Footer(){
+    return(
+        <div className="footer">
+            <hr/>
+            <footer className="footer-content">
+                <div>
+                    <span>Created by: Luis Renteria</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
