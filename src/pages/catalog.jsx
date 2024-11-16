3881a2df92e0963b8089db9d3de53a101478f61b
LereRentesca <esteban.renteria@uabc.edu.mx>
2024-11-16 11:51:41 -0800

    Class #02

---
+++ src/pages/catalog.jsx
@@ -0,0 +1,24 @@
+import "./styles/catalog.css";
+import Product from "../components/product";
+
+function Catalog(){
+    return(
+        <div className='content'>
+            <div>
+                <h1>This is where the catalog it's going to be.</h1>
+            </div>
+            <div>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+            </div>
+        </div>
+    );
+}
+
+export default Catalog;
